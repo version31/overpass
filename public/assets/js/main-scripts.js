28e1c0f7c0cc6ead5dcf8941cac6ff593a0933c2
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-11-15 16:48:48 +0330

    Added portfolio, updated portfolio carousel

--- public/assets/js/main-scripts.js
+++ public/assets/js/main-scripts.js
@@ -22,12 +22,44 @@
                     el: ".el-slider-pagination",
                     clickable: true,
                     renderBullet: function (index, className) {
-                        return '<div class="el-slider-dots-item ' + className + '"><p class="title"><span>' + title[index] + '</span><span>' + subtitle[index] + '</span></p><span class="icon"><svg data-index="' + index + '" viewBox="0 0 14 14" width="14" height="14" xmlns="http://www.w3.org/2000/svg"><circle cx="7" cy="7" r="2.5" class="chip"></circle><circle fill="none" stroke-width="2" stroke-miterlimit="10" cx="7" cy="7" r="6" class="progress-1"></circle><circle fill="none" stroke-width="2" stroke-miterlimit="10" cx="7" cy="7" r="6" class="progress-2"></circle></svg></span></div>';
+                        return '<div class="el-slider-dots-item ' + className + '"><p class="title"><span>' + title[index] + "</span><span>" + subtitle[index] + '</span></p><span class="icon"><svg data-index="' + index + '" viewBox="0 0 14 14" width="14" height="14" xmlns="http://www.w3.org/2000/svg"><circle cx="7" cy="7" r="2.5" class="chip"></circle><circle fill="none" stroke-width="2" stroke-miterlimit="10" cx="7" cy="7" r="6" class="progress-1"></circle><circle fill="none" stroke-width="2" stroke-miterlimit="10" cx="7" cy="7" r="6" class="progress-2"></circle></svg></span></div>';
                     },
                 },
                 navigation: {
                     prevEl: ".el-slider-prev",
                     nextEl: ".el-slider-next",
+                },
+            });
+
+            var mySwiper = new Swiper(".el-swiper-portfolio-3", {
+                loop: false,
+                navigation: {
+                    nextEl: ".el-slider-next",
+                    prevEl: ".el-slider-prev",
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1.2,
+                        spaceBetween: 12,
+                    },
+                    576: {
+                        slidesPerView: 1.5,
+                        spaceBetween: 12,
+                    },
+                    768: {
+                        slidesPerView: 1.8,
+                        spaceBetween: 18,
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 18,
+                    },
+                    1200: {
+                        loop: true,
+                        spaceBetween: 20,
+                        slidesPerView: 2.01,
+                        // centeredSlides: true,
+                    },
                 },
             });
         },
